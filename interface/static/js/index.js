8022f2a682825a6a5453531693855f584e41f919
haakensonb <untamedhawk44@gmail.com>
2020-02-29 13:15:19 -0500

    Able to render simple dag

--- interface/static/js/index.js
+++ interface/static/js/index.js
@@ -260,3 +260,87 @@ async function getData(url){
 
 getAndRenderPrivs();
 getAndRenderRolesAndPrivs();
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    let cy = cytoscape({
+        container: document.getElementById("cy"),
+    
+        elements: [ // list of graph elements to start with
+            { // node a
+              data: { id: 'a' }
+            },
+            { // node b
+              data: { id: 'b' }
+            },
+            { // edge ab
+              data: { id: 'ab', source: 'a', target: 'b' }
+            }
+          ],
+        
+            style: [ // the stylesheet for the graph
+                {
+                selector: 'node',
+                style: {
+                    'background-color': '#666',
+                    'label': 'data(id)'
+                }
+                },
+            
+                {
+                selector: 'edge',
+                style: {
+                    'width': 3,
+                    'line-color': '#ccc',
+                    'target-arrow-color': '#ccc',
+                    'target-arrow-shape': 'triangle',
+                    'curve-style': 'bezier'
+                    }
+                }
+            ],
+        
+            layout: {
+                name: 'dagre'
+            },
+
+            minZoom: 1e-1,
+            maxZoom: 1e1,
+            zoomingEnabled: true,
+            userPanningEnabled: true,
+            boxSelectionEnabled: true,
+            selectionType: 'single',
+            touchTapThreshold: 8,
+            desktopTapThreshold: 4,
+            autolock: false,
+            autoungrabify: false,
+            autounselectify: false,
+    });
+
+    let dagreDefaults  = {
+        name: "dagre",
+        // dagre algo options, uses default value on undefined
+        nodeSep: undefined, // the separation between adjacent nodes in the same rank
+        edgeSep: undefined, // the separation between adjacent edges in the same rank
+        rankSep: undefined, // the separation between each rank in the layout
+        rankDir: undefined, // 'TB' for top to bottom flow, 'LR' for left to right,
+        ranker: undefined, // Type of algorithm to assign a rank to each node in the input graph. Possible values: 'network-simplex', 'tight-tree' or 'longest-path'
+        minLen: function( edge ){ return 1; }, // number of ranks to keep between the source and target of the edge
+        edgeWeight: function( edge ){ return 1; }, // higher weight edges are generally made shorter and straighter than lower weight edges
+      
+        // general layout options
+        fit: true, // whether to fit to viewport
+        padding: 30, // fit padding
+        spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
+        nodeDimensionsIncludeLabels: false, // whether labels should be included in determining the space used by a node
+        animate: false, // whether to transition the node positions
+        animateFilter: function( node, i ){ return true; }, // whether to animate specific nodes when animation is on; non-animated nodes immediately go to their final positions
+        animationDuration: 500, // duration of animation in ms if enabled
+        animationEasing: undefined, // easing of animation if enabled
+        boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+        transform: function( node, pos ){ return pos; }, // a function that applies a transform to the final node position
+        ready: function(){}, // on layoutready
+        stop: function(){} // on layoutstop
+      };
+
+      cy.layout(dagreDefaults).run();
+});
